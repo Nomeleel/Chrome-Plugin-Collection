8a5c6665454d4b11e7553c1bf50b5e3c8dd0e1d8
Nomeleel <choonlay@outlook.com>
2021-09-24 00:23:10 +0800

    Refine fillValueInSnippet, use recursion.

--- MidasTouch/src/util/snippet.ts
+++ MidasTouch/src/util/snippet.ts
@@ -11,8 +11,18 @@ function removeUnuseSnippetField(str: string): string {
   return str.replace(/(?<=\n)\t*\S*:\s?\S*\$\{\S*\}\S*\n/g, '');
 }
 
-export function fillValueInSnippet(type: string, snippetMap: Map<string, Array<string> | string>, dataMap: Map<string, any>): string {
+export function fillValueInSnippet(type: string, snippetMap: Map<string, Array<string> | string>, dataSource: Map<string, any> | string): string {
   let template = getSnippet(snippetMap, type);
-  let text = template.replace(/\$\{\S*\}/g, str => dataMap.get(str.slice(2, -1)) ?? str);
+  let text = template.replace(/\$\{\S*\}/g, str => {
+    if (typeof dataSource === 'string') return dataSource;
+
+    let field = str.slice(2, -1);
+    if (field.includes(':')) {
+      let pair = field.split(':');
+      let value = dataSource.get(pair[0]);
+      return value ? fillValueInSnippet(pair[1], snippetMap, value) : str;
+    }
+    return dataSource.get(field) ?? str;
+  });
   return removeUnuseSnippetField(text);
 }
