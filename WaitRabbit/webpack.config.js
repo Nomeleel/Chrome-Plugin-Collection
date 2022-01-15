8a878ee3662d68098c3367c9c756028ac2fe29c8
Nomeleel <choonlay@outlook.com>
2022-01-15 17:53:21 +0800

    Add Wait Rabbit init.

---
+++ WaitRabbit/webpack.config.js
@@ -0,0 +1,40 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.css', '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'wait_rabbit.js',
+    path: path.resolve(__dirname, 'bin')
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  }
+};
