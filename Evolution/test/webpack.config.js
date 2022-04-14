e09ea3220118794bc07de5f48d442e6c22e937c1
Nomeleel <choonlay@outlook.com>
2022-04-14 15:50:26 +0800

    Start evolution.

---
+++ Evolution/test/webpack.config.js
@@ -0,0 +1,42 @@
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+
+module.exports = {
+  entry: './test/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.css', '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'evolution.js',
+    path: __dirname
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  },
+  plugins: [
+    new MiniCssExtractPlugin()
+  ]
+};
---
+++ Evolution/webpack.config.js
@@ -0,0 +1,41 @@
+const path = require('path');
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
+const CopyPlugin = require('copy-webpack-plugin');
+
+module.exports = {
+  entry: './src/index.ts',
+  devtool: 'inline-source-map',
+  mode: 'development',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/
+      },
+      {
+        test: /\.css$/,
+        use: [MiniCssExtractPlugin.loader, 'css-loader'],
+      }
+    ]
+  },
+  resolve: {
+    extensions: [ '.css', '.tsx', '.ts', '.js' ]
+  },
+  output: {
+    filename: 'evolution.js',
+    path: path.resolve(__dirname, 'bin')
+  },
+  optimization: {
+    splitChunks: {
+      cacheGroups: {
+        styles: {
+          name: 'styles',
+          test: /\.css$/,
+          chunks: 'all',
+          enforce: true,
+        },
+      },
+    },
+  }
+};
