b005d34cd9101dd38bdcdbb7ee2f0d77c26b034a
Nomeleel <choonlay@outlook.com>
2022-04-19 22:01:24 +0800

    Html table parse table body.

--- Evolution/test/table/index.ts
+++ Evolution/test/table/index.ts
@@ -7,7 +7,24 @@ window.onload = () => {
     text.innerText = '导出'
     btn.appendChild(text);
     btn.onclick = () => {
-      console.log(`table.border: ${table.border}`);
+      // Use first body
+      if (table.tBodies?.item(0)?.rows?.item(0)) {
+        let body = table.tBodies[0].rows;
+        let hasHead = table.tHead?.rows?.item(0) != null;
+        let head = hasHead ? table.tHead?.rows?.item(0) : body[0]
+        
+        for (let index = hasHead ? 0 : 1; index < body.length; index++) {
+          console.log(`body row: ${Array.from(body[index].cells).map((cell) => cell.innerText)}`);
+          // for (const cell of body[index].cells) {
+          //   console.log(`cell: ${cell.innerText}`);
+          // }
+        }
+
+        // console.log(`table head: ${head.cells}`);
+        // console.log(`table body length: ${table.tBodies[0].rows.length}`);
+        // console.log(`table rows length: ${table.rows.length}`);
+      }
+
     }
 
     (table.parentNode ?? table).appendChild(btn);
